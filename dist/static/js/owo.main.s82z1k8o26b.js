fd24ceeeb5bee1956974b2ca791e0b48eaaab103
puge <mail@puge.org>
2019-12-26 09:45:28 +0800

    save

--- dist/static/js/owo.main.s82z1k8o26b.js
+++ dist/static/js/owo.main.s82z1k8o26b.js
@@ -1,4 +1,4 @@
-// Wed Dec 25 2019 17:16:21 GMT+0800 (GMT+08:00)
+// Thu Dec 26 2019 09:13:07 GMT+0800 (GMT+08:00)
 var owo = {tool: {},state: {},};
 /* 方法合集 */
 var _owo = {}
@@ -277,23 +277,6 @@ owo.tool.each = function (obj, fn) {
 }
 
 
-
-
-// 这是用于代码调试的自动刷新代码，他不应该出现在正式上线版本!
-if ("WebSocket" in window) {
-  // 打开一个 web socket
-  if (!window._owo.ws) window._owo.ws = new WebSocket("ws://" + window.location.host)
-  window._owo.ws.onmessage = function (evt) { 
-    if (evt.data == 'reload') {
-      location.reload()
-    }
-  }
-  window._owo.ws.onclose = function() { 
-    console.info('与服务器断开连接')
-  }
-} else {
-  console.error('浏览器不支持WebSocket')
-}
 
 
 
